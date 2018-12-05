5575e0f08b5a441dc1ec310d03eeec6e527af6c9
ALEJANDRO GARCIA <galejandrol88@gmail.com>
2018-12-05 11:33:17 -0300

    Primer commit

---
+++ src/service/tweet_manager_test.go
@@ -0,0 +1,16 @@
+package service_test
+
+import (
+	"testing"
+	"github.com/alejandroagarcia/twitter/src/service"
+)
+
+func TestPublishedTweetIsSaved(t *testing.T){
+	var tweet string = "this is my first tweet"
+
+	service.PublishTweet(tweet)
+
+	if service.Tweet != tweet {
+		t.Error("Expected tweet is", tweet)
+	}
+}
